3457c8a7739f68c0952bbd93c283efa6e03b386e
Akhil Narang <akhil@akhilnarang.me>
2016-09-12 10:05:14 +0530

    bullhead: no more prebuilt, switch to inline kronic
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,11 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bullhead boot_cpus=0-5
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 msm_poweroff.download_mode=0
 
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/lge/bullhead
+TARGET_KERNEL_CONFIG := kronic_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 BOARD_USES_ALSA_AUDIO := true
