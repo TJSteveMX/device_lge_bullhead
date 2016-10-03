5806cbaa00dd838e766e1c6a8a8762a5352a5af2
TJSteveMX <ec.lento@gmail.com>
2016-10-03 10:21:34 -0700

    Create tesla.mk

---
+++ tesla.mk
@@ -0,0 +1,20 @@
+# Inherit AOSP device configuration for bullhead.
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+
+# Setup device specific product configuration.
+
+PRODUCT_NAME := tesla_bullhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := bullhead
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_MANUFACTURER := LGE
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/NRD90S/3142244:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NRD90S 3142244 release-keys" \
+    BUILD_ID=NRD90S
