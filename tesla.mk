a3952e21f17f6d74603ddb6bb380067d0fb0ec79
TJSteveMX <ec.lento@gmail.com>
2016-10-06 15:04:24 -0700

    Update tesla.mk

--- tesla.mk
+++ tesla.mk
@@ -1,16 +1,22 @@
-# Inherit AOSP device configuration for bullhead.
-$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
 
 # Inherit common product files.
 $(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
-# Setup device specific product configuration.
+# Enhanced NFC
+$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := tesla_bullhead
 PRODUCT_BRAND := google
-PRODUCT_DEVICE := bullhead
 PRODUCT_MODEL := Nexus 5X
-PRODUCT_MANUFACTURER := LGE
+TARGET_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
